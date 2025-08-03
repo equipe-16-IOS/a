d413ed09bfc749c0089064fcee6f41bd160abb6d
equipe-16-IOS <equipe16.capacitaios@gmail.com>
2025-08-03 20:27:58 -0300

    Tela de adiconar procutos

--- Aplicativo2/View/AdicionarProdutoView.swift
+++ Aplicativo2/View/AdicionarProdutoView.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 struct AdicionarProdutoView: View {
     @ObservedObject var viewModel: AddProductViewModel = .shared
-    @State var nome = ""
-    @State var calorias = 0
-    @State var preco = 0.0
-    @State var validade: Date?
-    @State var imagem: Data?
+    @State private var nomeProduto = ""
+    @State var calorias = ""
+    @State private var preco = ""
+    @State private var validade: Date = Date()
+    @State private var categoria = ""
+    @State private var imagem: Data?
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
@@ -37,12 +38,44 @@ struct AdicionarProdutoView: View {
                     
                 }
                 .padding()
-                
-                Text("Adicionar Produto")
-                    .font(.title2)
-                    .bold()
-                    .foregroundColor(.blue)
+                           Text("Adicionar Produto")
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(.blue)
+                    Spacer()
+                    
+                VStack(alignment: .leading, spacing: 18){
+                    
+                    //NOME DO PRODUTO
+                    Text( "Nome do Produto")
+                    TextField("Ex: Macarrão Parafuso", text: $nomeProduto)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    //CALORIAS
+                    Text( "Calorias")
+                    TextField("Ex: 250.00kcal", text: $calorias)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    //PREÇO
+                    Text( "Preço")
+                    TextField("Ex: R$ 15.00", text: $preco)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    //VALIDADE
+                    DatePicker("Validade", selection: $validade, displayedComponents: .date)
+                        .datePickerStyle(.compact)
+                    
+                    //CATEGORIAS
+                    
+                    Text("Categorias")
+                    TextField("Ex: Frutas", text: $categoria)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    }
                 Spacer()
+                //TODO ADD BOTÃO
+                //BOTÃO
+              
             }
             .navigationBarBackButtonHidden(true)
         }
@@ -51,4 +84,3 @@ struct AdicionarProdutoView: View {
     #Preview {
         AdicionarProdutoView()
     }
-
