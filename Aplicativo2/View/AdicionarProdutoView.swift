986b2d3b291faa6a6a75a00393efa143606c64bd
equipe-16-IOS <equipe16.capacitaios@gmail.com>
2025-08-03 20:06:26 -0300

    .

--- Aplicativo2/View/AdicionarProdutoView.swift
+++ Aplicativo2/View/AdicionarProdutoView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct AdicionarProdutoView: View {
     @ObservedObject var viewModel: AddProductViewModel = .shared
     @State private var nomeProduto = ""
-    @State var calorias = ""
+    @State private var calorias = ""
     @State private var preco = ""
     @State private var validade: Date = Date()
     @State private var categoria = ""
@@ -74,7 +74,6 @@ struct AdicionarProdutoView: View {
                     }
                 Spacer()
                 //TODO ADD BOTÃO
-                //BOTÃO
               
             }
             .navigationBarBackButtonHidden(true)
