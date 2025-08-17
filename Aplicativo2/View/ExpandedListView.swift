5b9af4fa9bd0b629689ed4ee3e1a443630cc3cfd
equipe-16-IOS <equipe16.capacitaios@gmail.com>
2025-08-17 09:54:10 -0300

    adição de novo arquivo ExpandedListView

---
+++ Aplicativo2/View/ExpandedListView.swift
@@ -0,0 +1,33 @@
+//
+//
+//  ExpandedListView.swift
+//  Aplicativo2
+//
+//  Created by Izadora Montenegro on 13/08/25.
+//
+
+import SwiftUI
+
+struct ExpandedListView: View {
+    let list: ShoppingList
+    
+    let columns = [
+        GridItem(.flexible(), spacing: 16),
+        GridItem(.flexible(), spacing: 16),
+    ]
+    
+    var body: some View {
+        VStack {
+            
+            ScrollView(.vertical) {
+                LazyVGrid(columns: columns) {
+                    ForEach(list.products) { product in
+                            Text(product.name)
+                        
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
